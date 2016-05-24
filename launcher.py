c3c8fade3e5f9fcc7944b78e3f8e58b6c6e14c4b
mortenflood <mortenflood@hotmail.com>
2016-05-23 17:24:04 -0700

    Update Morten

--- launcher.py
+++ launcher.py
@@ -9,16 +9,25 @@ path2 = "testset.txt"
 
 def launch(file):
     print "Start"
-    # training_data, training_labels, test_data, test_labels = parser.load_dataset_decades_zero_index(file)
-    training_data, training_labels, test_data, test_labels = parser.load_dataset_zero_index(file)
+    training_data, training_labels, test_data, test_labels = parser.load_dataset_decades_zero_index(file)
+    # training_data, training_labels, test_data, test_labels = parser.load_dataset_zero_index(file)
 
     print "Dataset loaded"
 
     size = training_data.shape[0]
-    x = training_data[range(size-5000)]
-    y = training_labels[range(size-5000)]
-    x_val = training_data[range(size-5000, size)]
-    y_val = training_labels[range(size-5000, size)]
+    x = training_data[range(size-10000)]
+    y = training_labels[range(size-10000)]
+    x_val = training_data[range(size-10000, size)]
+    y_val = training_labels[range(size-10000, size)]
+
+    print "Normalize"
+    x = parser.mean_subtraction(x, x)
+    x_val = parser.mean_subtraction(x_val, x)
+    test_data = parser.mean_subtraction(test_data, x)
+
+    # x = parser.normalize(x, x)
+    # x_val = parser.normalize(x_val, x)
+    # test_data = parser.normalize(test_data, x)
 
     print "Start training ..."
 
